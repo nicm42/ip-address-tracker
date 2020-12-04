82f1e4aeccc653aa0ef3e0a9dd8fda8067f17452
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-04 18:06:04 +0000

    Added in the basics to get the location data from the api

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
 const myKey = config.myKey;
+const ip = '8.8.8.8';
+
+async function getData(ip) {
+  const request = await fetch('https://geo.ipify.org/api/v1' + 'apiKey=' + myKey + '&ipAddress=' + ip);
+  const data = await request.jason();
+  return data;
+}
+
+getData(ip)
+  .then(data => console.log(data));
