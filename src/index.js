c9a0330b815bf40d9a4abcf0bf3a9d81add08ec6
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-06 12:23:41 +0000

    Made the region and UTC data the same format as in the design

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ getData(ip)
   //.then(data => console.log(data));
   .then(data => {
     console.log(data.ip);
-    console.log(data.location.city, data.location.region, data.location.postalCode, data.location.country);
-    console.log(data.location.timezone);
+    //console.log(data.location.city, data.location.region, data.location.postalCode, data.location.country);
+    //If we're in the USA then convert region to two letters
+    let region = data.location.region;
+    if(data.location.country === 'US') {
+      region = 'CA';
+    }
+    console.log(data.location.city, region, data.location.postalCode)
+    console.log('UTC ' + data.location.timezone);
     console.log(data.isp);
-  })
+  });
+
