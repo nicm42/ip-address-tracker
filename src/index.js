c44cb58f1a6b41b447cc88ea65c231cc715acf0e
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-06 12:26:47 +0000

    Moved the data outputs to a separate function

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ const ip = '8.8.8.8';
 getData(ip)
   //.then(data => console.log(data));
   .then(data => {
-    console.log(data.ip);
-    //console.log(data.location.city, data.location.region, data.location.postalCode, data.location.country);
-    //If we're in the USA then convert region to two letters
-    let region = data.location.region;
-    if(data.location.country === 'US') {
-      region = 'CA';
-    }
-    console.log(data.location.city, region, data.location.postalCode)
-    console.log('UTC ' + data.location.timezone);
-    console.log(data.isp);
+    outputData(data);
   });
 
+function outputData(data) {
+  console.log(data.ip);
+  //If we're in the USA then convert region to two letters
+  let region = data.location.region;
+  if(data.location.country === 'US') {
+    region = 'CA';
+  }
+  console.log(data.location.city, region, data.location.postalCode)
+  console.log('UTC ' + data.location.timezone);
+  console.log(data.isp);
+}
