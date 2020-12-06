6690a8ad99661e96701f27b4b357797e4119717e
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-06 11:01:12 +0000

    Got the location APi to pick up all the information we need

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ const ip = '8.8.8.8';
 const myKey = config.myKey;
 
 async function getData(ip) {
-  const request = await fetch('https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?' + 'apiKey=' + myKey + '&ipAddress=' + ip);
-  const data = await request.json();
-  return data;
+  try {
+    const request = await fetch('https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?' + 'apiKey=' + myKey + '&ipAddress=' + ip);
+    const data = await request.json();
+    return data;
+  } catch (error) {
+    console.log(error); //TODO something with the error
+  }
 }
 
 getData(ip)
-  .then(data => console.log(data));
+  //.then(data => console.log(data));
+  .then(data => {
+    console.log(data.ip);
+    console.log(data.location.city, data.location.region, data.location.postalCode);
+    console.log(data.location.timezone);
+    console.log(data.isp);
+  })
