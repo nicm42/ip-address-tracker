5917791d2e5023596c160dfb56b1d0bfc29b3fcd
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-12 17:57:04 +0000

    Set it up to test it with the api

--- src/getData.js
+++ src/getData.js
@@ -2,8 +2,10 @@ import { config } from "./config";
 const myKey = config.myKey;
 
 export async function getData(ip) {
+  console.log(ip);
   try {
     const request = await fetch('https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?' + 'apiKey=' + myKey + '&ipAddress=' + ip);
+    //const request = await fetch('https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?' + 'apiKey=' + myKey);
     const data = await request.json();
     return data;
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import {getData} from './getData';
 import {showMap} from './showMap';
 import {showData} from './showData';
 
-const ip = '8.8.8.8';
-//const ip = '';
+//const ip = '8.8.8.8';
+const ip = '';
 
 //TODO commented out to stop it doing API requests when I'm doing other things
-/* getData(ip)
+getData(ip)
   //.then(data => console.log(data));
   .then(data => {
     processData(data);
-  }); */
+  });
 
 function processData(data) {
   //If we're in the USA then convert region to two letters
@@ -19,19 +19,19 @@ function processData(data) {
   if(data.location.country === 'US') {
     region = 'CA';
   }
-  //console.log(data.ip);
-  //console.log(data.location.city, region, data.location.postalCode)
-  //console.log('UTC ' + data.location.timezone);
-  //console.log(data.isp);
-  //console.log(data.location.lat, data.location.lng);
+  console.log(data.ip);
+  console.log(data.location.city, region, data.location.postalCode)
+  console.log('UTC ' + data.location.timezone);
+  console.log(data.isp);
+  console.log(data.location.lat, data.location.lng);
   const place = [data.location.city, region, data.location.postalCode];
   const timezone = `UTC ${data.location.timezone}`;
   const isp = data.isp;
-  showData(ip, place, timezone, isp);
+  showData(data.ip, place, timezone, isp);
   showMap(data.location.lat, data.location.lng);
 }
 
 //TODO comment out
 //To test if not using the API
-showData('192.212.174.101', ['Brooklyn','NY','10001'], 'UTC -05:00', 'SpaceX Starlink');
-showMap(52.374990, -1.556640);
+//showData('192.212.174.101', ['Brooklyn','NY','10001'], 'UTC -05:00', 'SpaceX Starlink');
+//showMap(52.374990, -1.556640);
