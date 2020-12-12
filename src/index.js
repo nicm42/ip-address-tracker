ddea83059310cf58acf52fc937b1fcd144d36741
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-12 16:52:06 +0000

    Show the data in the divs

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 import './styles.scss';
 import {getData} from './getData';
 import {showMap} from './showMap';
+import {showData} from './showData';
 
-import L from 'leaflet';
-L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
-import 'leaflet/dist/leaflet.css';
-/* This code is needed to properly load the images in the Leaflet CSS */
- delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
-//const ip = '8.8.8.8';
-const ip = '';
+const ip = '8.8.8.8';
+//const ip = '';
 
 //TODO commented out to stop it doing API requests when I'm doing other things
 /* getData(ip)
   //.then(data => console.log(data));
   .then(data => {
-    outputData(data);
+    processData(data);
   }); */
 
-function outputData(data) {
-  console.log(data.ip);
+function processData(data) {
   //If we're in the USA then convert region to two letters
   let region = data.location.region;
   if(data.location.country === 'US') {
     region = 'CA';
   }
-  console.log(data.location.city, region, data.location.postalCode)
-  console.log('UTC ' + data.location.timezone);
-  console.log(data.isp);
-  console.log(location.lat, location.lng);
-  showMap(location.lat, location.lng);
+  //console.log(data.ip);
+  //console.log(data.location.city, region, data.location.postalCode)
+  //console.log('UTC ' + data.location.timezone);
+  //console.log(data.isp);
+  //console.log(data.location.lat, data.location.lng);
+  const place = [data.location.city, region, data.location.postalCode];
+  const timezone = `UTC ${data.location.timezone}`;
+  const isp = data.isp;
+  showData(ip, place, timezone, isp);
+  showMap(data.location.lat, data.location.lng);
 }
 
+//TODO comment out
 //To test if not using the API
+showData('192.212.174.101', ['Brooklyn','NY','10001'], 'UTC -05:00', 'SpaceX Starlink');
 showMap(52.374990, -1.556640);
