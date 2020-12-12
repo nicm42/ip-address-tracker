b7c5a41a18889f9557580cb8d4291ab6b055d3fd
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-12 16:51:35 +0000

    Added map imports to the showMap file

--- src/showMap.js
+++ src/showMap.js
@@ -1,3 +1,15 @@
+import L from 'leaflet';
+
+L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
+import 'leaflet/dist/leaflet.css';
+/* This code is needed to properly load the images in the Leaflet CSS */
+ delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 export function showMap(lat, lng) {
   const map = L.map('map').setView([lat, lng], 13);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
