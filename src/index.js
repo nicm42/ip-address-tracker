6373b88077d60e97595231ab84c2b235b5a62d22
Nic <42087659+nicm42@users.noreply.github.com>
2020-12-18 16:35:59 +0000

    Turned the API back on

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {findData} from './findData';
 
 let ip = '';
 let domain = '';
-//findData(ip, domain);
+findData(ip, domain);
 
 const search = document.querySelector('.search');
 search.addEventListener('submit', function(event) {
@@ -22,18 +22,16 @@ search.addEventListener('submit', function(event) {
 
   }
   ////console.log('IP is ' + ip);
-  //findData(ip, domain);
-  //TODO comment out
+  findData(ip, domain);
   //To test if not using the API
-  showData('0.0.0.0', ['London','England',''], 'UTC +00:00', 'My ISP');
-  showMap(-33.868820, 151.209290);
+  //showData('0.0.0.0', ['London','England',''], 'UTC +00:00', 'My ISP');
+  //showMap(-33.868820, 151.209290);
 })
 
 
-//TODO comment out
 //To test if not using the API
-import {showData} from './showData';
+/* import {showData} from './showData';
 import {showMap} from './showMap';
 
 showData('192.212.174.101', ['Brooklyn','NY','10001'], 'UTC -05:00', 'SpaceX Starlink');
-showMap(52.374990, -1.556640);
+showMap(52.374990, -1.556640); */
